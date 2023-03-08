616be5f9443213d5f42f39a23899f09f1cd1adb5
Darran Holmes <darranholmes74@gmail.com>
2023-03-08 10:53:22 -0800

    added chakra to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
-
+import { ChakraProvider } from '@chakra-ui/react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+	<ChakraProvider>
 		<Auth0Provider
 			domain={import.meta.env.VITE_AUTH_DOMAIN}
 			clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
@@ -19,5 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		>
 			<App />
 		</Auth0Provider>
+		</ChakraProvider>
 	</React.StrictMode>
 )
